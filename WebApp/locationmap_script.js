55846edd9190dcbb7f4bceca0fe594c69b2e722f
ITSupapitch <59070169@it.kmitl.ac.th>
2019-12-20 00:28:37 +0700

    update webapp

--- WebApp/locationmap_script.js
+++ WebApp/locationmap_script.js
@@ -40,7 +40,7 @@ function showPosition(position) {
  		new google.maps.LatLng(13.7250487802915, 100.7821217802915) // โคม
  		);
 
-	var u_latlng = new google.maps.LatLng(13.720799, 100.784680); //user's location
+	var u_latlng = new google.maps.LatLng(13.7275202, 100.7694236); //user's location
 	var check_jinda = zone_jinda.contains(u_latlng);
 	var check_keki = zone_keki.contains(u_latlng);
 	var check_fbt = zone_fbt.contains(u_latlng);	
@@ -58,10 +58,10 @@ function showPosition(position) {
 		y.innerHTML = '<div id="check_header" style="background-color: red;">คุณไม่อยู่ในพื้นที่ให้บริการในขณะนี้</div>';
 	}
 
-z.innerHTML = '<form action="tutorial.html"><input id="tutorial_btn" type="image" src="pic/info.png" alt="Submit" width="48" height="48"></form>';
+//z.innerHTML = '<form action="tutorial.html"><input id="tutorial_btn" type="image" src="pic/info.png" alt="Submit" width="48" height="48"></form>';
 
 	var locations = [
-	['สุกี้ดารา', 13.7221057, 100.7852154],
+	['สุกี้ดารา', 13.7221057, 100.7852154],//jinda (10)
 	['Pasta Home (พาสต้าโฮม)', 13.71779, 100.783517],
 	['เกี๊ยวกุ้งจินดา', 13.7212019, 100.7842203],
 	['Coffee Today คอฟฟี่ทูเดย์',13.7218673, 100.7843005],
@@ -71,30 +71,35 @@ z.innerHTML = '<form action="tutorial.html"><input id="tutorial_btn" type="image
 	['ครัวป้าเจ๊ก', 13.7197648, 100.7837982],
 	['R-HA (อาฮ่า)', 13.721437, 100.783842],
 	['ไอศครีมไข่แข็ง', 13.720562, 100.783833],
-	['ชาบู ปาร์ตี้ (Shabu party)', 13.7221422, 100.7807354],
-	['', , ],
-	['', , ],
-	['', , ],
-	['', , ],
-	['', , ],
-	['', , ],
-	['', , ],
-	['', , ],
-	['', , ],
-	['', , ],
-	['', , ],
-	['', , ],
-	['', , ],
-	['', , ],
-	['', , ],
-	['', , ],
-	['', , ],
-	['', , ],
-	];
+	['ชาบู ปาร์ตี้ (Shabu party)', 13.7221422, 100.7807354], //fbt (11)
+	['จินตภัทร์ เบเกอรี่', 13.722068, 100.780391],
+	['ชา กุมารทอง' , 13.723102, 100.7804381],
+	['พั้น~ซ์', 13.7216955, 100.780325],
+	['บ้านข้าวซอย', 13.7228489, 100.7807009 ],
+	['ร้านโคม', 13.7237173, 100.7807419],
+	['ละมุน (Lamoon)', 13.7225579, 100.7803574],
+	['แมวกินปลา@ลาดกระบัง',13.7228186, 100.7801383],
+	['สุขใจเตี๋ยวไข่ต้มยำ ', 13.7228773, 100.7803227],
+	['ไก่เกาหลี อูรี ชิกเก้นท์ 우리 치킨 สาขา ลาดกระบัง',13.7220672, 100.7804952],
+	['จุดสามจุด', 13.7228193, 100.7801028], 
+	['A&P ไก่ยำแซ่บ',13.7275202, 100.7694236], //keki (13)
+	['Daily delivery KMITL',13.7278467,100.7701097],
+	['Kinnii กินนี่',13.7278592,100.7703925],
+	['Shogun ลาดกระบัง',13.7277267,100.7702708],
+	['ฅน 8 หน้า SUSHI BAR & RESTAURANT',13.7275384,100.7696328],
+	['ชาชักโกอิน',13.7273668,100.769545],
+	['ญาแฝดข้าวมันไก่ ซ.เกกี4',13.7273834,100.7694829],
+	['มุมสบาย',13.7269879,100.7698375],
+	['วัวล้วนๆ ไม่มีควายผสม สาขา ลาดกะบัง KMITL',13.7277895,100.7701018],
+	['หมูทอดป้าจุก',13.7270951,100.7702075],
+	['อิ่มแปล้ แฮปปี้คัพ',13.727737,100.769764],
+	['เสต็ก อิ่มเอม',13.7271134,100.7703543],
+	['STEAK TECHNO สเต็ก เทคโนฯ',13.727815,100.769738]
+	]; //total = 34 places
 
 	var mapOptions = {
 		center: u_latlng,
-		zoom: 16.3,
+		zoom: 16.5,
 		mapTypeId: 'terrain',
 		zoomControl: false,
 		mapTypeControl: false,
@@ -102,6 +107,32 @@ z.innerHTML = '<form action="tutorial.html"><input id="tutorial_btn" type="image
 		fullscreenControl: false,
 		streetViewControl: false,
 	}
+
+	
+	var ct1_marker ='<div class="row">'+
+  '<div class="col-3">'+
+  	'<img id="icon_marker" src="pic/coffee-cup.png">'+
+  '</div>'+
+  '<div class="col-9">'+
+  	'<div id="grid_marker" class="row">'+
+			'<div class="col-sm-6">'+
+				'<h4 class="modal-title" id="name_marker">';
+
+    var ct2_marker = '</h4>'+
+              '</div>'+
+              '<div class="col-sm-6" id="header_marker">'+
+               '<div class="mk_rating">'+            
+                '<img class="mk_star" src="pic/get_star.png">'+
+                '<img class="mk_star" src="pic/get_star.png">'+
+                '<img class="mk_star" src="pic/get_star.png">'+
+                '<img class="mk_star" src="pic/get_star.png">'+
+                '<img class="mk_star" src="pic/nopoint.png">'+
+                '<h6 class="total_marker">4.0</h6>'+
+              '</div>'+
+            '</div>'+
+          '</div><h6 id="status_marker"> &#8226; เปิดอยู่ในขณะนี้</h6></div>';
+
+     var btn_marker = '<button type="button" class="btn btn-block btn-info" data-toggle="modal" data-target="#exampleModalLong">อ่านต่อ</button>';
 
 	var maps = new google.maps.Map(document.getElementById("map"),mapOptions);
 
@@ -119,8 +150,7 @@ z.innerHTML = '<form action="tutorial.html"><input id="tutorial_btn" type="image
 
 		google.maps.event.addListener(marker, 'click', (function(marker, i) {
 			return function() {
-				info.setContent('<div style="font-size: 18px;color: purple";text-align:center>'+locations[i][0]+'</div>'
-					+'<button type="button" class="btn edit_reslist" data-toggle="modal" data-target="#exampleModalLong">อ่านต่อ</button>');
+				info.setContent(ct1_marker+locations[i][0]+ct2_marker+btn_marker);
 				info.open(maps, marker);
 			}
 		})(marker, i));
@@ -128,7 +158,7 @@ z.innerHTML = '<form action="tutorial.html"><input id="tutorial_btn" type="image
 	}
 
 	var u_marker = new google.maps.Marker({
-		position: new google.maps.LatLng(13.720799, 100.784680),
+		position: new google.maps.LatLng(13.7275202, 100.7694236), //user's location
 		map: maps,
 		title: 'ตำแหน่งผู้ใช้',
 		animation: google.maps.Animation.BOUNCE,
