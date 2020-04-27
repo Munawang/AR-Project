ed4d031cea1d90f834acf63f942c223582dee754
Munawang <59070036@it.kmitl.ac.th>
2020-04-27 10:04:52 +0700

    get rest-name from 3 zones

--- WebApp/index-multiple.js
+++ WebApp/index-multiple.js
@@ -56,7 +56,7 @@ function checkZone(position) {
         new google.maps.LatLng(13.7250487802915, 100.7821217802915) // โคม
     );
 
-    var u_latlng = new google.maps.LatLng(u_latitude, u_longitude); //**user's location
+    var u_latlng = new google.maps.LatLng(13.722719, 100.780227); //**user's location
     // For test: Jinda = 13.720278, 100.783761
     // For test: Keki = 13.727822, 100.769886
     // For test: FBT = 13.722719, 100.780227
@@ -66,12 +66,67 @@ function checkZone(position) {
     var check_keki = zone_keki.contains(u_latlng);
     var check_fbt = zone_fbt.contains(u_latlng);
 
+    var locations = [
+        ['Sumfruit บิงซู ลาดกระบัง', 13.7275256, 100.7708011, 'zone01', 'keki01'], //keki (11)
+        ['Daily delivery KMITL', 13.7278467, 100.7701097, 'zone01', 'keki02'],
+        ['มานีมีนมลาดกระบัง', 13.7277224, 100.7704063, 'zone01', 'keki03'],
+        ['STEAK TECHNO สเต็ก เทคโนฯ', 13.727815, 100.769738, 'zone01', 'keki04'],
+        ['Shogun ลาดกระบัง', 13.7277267, 100.7702708, 'zone01', 'keki05'],
+        ['ฅน 8 หน้า SUSHI BAR & RESTAURANT', 13.7275384, 100.7696328, 'zone01', 'keki06'],
+        ['ชาชักโกอิน', 13.7273668, 100.769545, 'zone01', 'keki07'],
+        ['ญาแฝดข้าวมันไก่ ซ.เกกี4', 13.7273834, 100.7694829, 'zone01', 'keki08'],
+        ['Hand Burger', 13.7277198, 100.770279, 'zone01', 'keki09'],
+        ['วัวล้วนๆ ไม่มีควายผสม สาขา ลาดกะบัง KMITL', 13.7277895, 100.7701018, 'zone01', 'keki10'],
+        ['เสต็ก อิ่มเอม', 13.7271134, 100.7703543, 'zone01', 'keki11'],
+        ['BKK Grill', 13.7213184, 100.7836547, 'zone02', 'jinda01'], //jinda (10)
+        ['Coffee Today คอฟฟี่ทูเดย์', 13.7218673, 100.7843005, 'zone02', 'jinda02'],
+        ['ICE FEELING', 13.7215943, 100.7836567, 'zone02', 'jinda03'],
+        ['ครัวฅนเมือง', 13.7214214, 100.7836731, 'zone02', 'jinda04'],
+        ['เดอะ พิซซ่า คอมปะนี', 13.7218051, 100.7835085, 'zone02', 'jinda05'],
+        ['Pasta Home (พาสต้าโฮม)', 13.71779, 100.783517, 'zone02', 'jinda06'],
+        ['สุกี้ดารา', 13.7221057, 100.7852154, 'zone02', 'jinda07'],
+        ['R-HA (อาฮ่า)', 13.721437, 100.783842, 'zone02', 'jinda08'],
+        ['เกี๊ยวกุ้งจินดา', 13.7212019, 100.7842203, 'zone02', 'jinda09'],
+        ['ไอศครีมไข่แข็ง', 13.720562, 100.783833, 'zone02', 'jinda10'],
+        ['ชาบู ปาร์ตี้ (Shabu party)', 13.7221422, 100.7807354, 'zone03', 'fbt01'], //fbt (10)
+        ['จินตภัทร์ เบเกอรี่', 13.722068, 100.780391, 'zone03', 'fbt02'],
+        ['ชา กุมารทอง', 13.723102, 100.7804381, 'zone03', 'fbt03'],
+        ['บ้านข้าวซอย', 13.7228489, 100.7807009, 'zone03', 'fbt04'],
+        ['พั้น~ซ์', 13.7216955, 100.780325, 'zone03', 'fbt05'],
+        ['ร้านโคม', 13.7237173, 100.7807419, 'zone03', 'fbt06'],
+        ['ละมุน (Lamoon)', 13.7225579, 100.7803574, 'zone03', 'fbt07'],
+        ['สุขใจเตี๋ยวไข่ต้มยำ ', 13.7228773, 100.7803227, 'zone03', 'fbt08'],
+        ['แมวกินปลา@ลาดกระบัง', 13.7228186, 100.7801383, 'zone03', 'fbt09'],
+        ['ไก่เกาหลี อูรี ชิกเก้นท์ 우리 치킨 สาขา ลาดกระบัง', 13.7220672, 100.7804952, 'zone03', 'fbt10']
+    ]; //total = 31 places
+
+
+
     if (check_jinda == true) {
-        console.log('ตอนนี้คุณอยู่ในโซน jinda');
+        console.log('ตอนนี้คุณอยู่ในโซนจินดา');
+        for (let i = 0; i < locations.length; i++) {
+            if (locations[i][3] == 'zone02') {
+                //console.log('zone 2 จินดา');
+                //console(locations[i][4])
+                console.log(locations[i][0])
+            }
+        }
     } else if (check_keki == true) {
-        console.log('ตอนนี้คุณอยู่ในโซน keki');
+        console.log('ตอนนี้คุณอยู่ในโซนเกกี');
+        for (let i = 0; i < locations.length; i++) {
+            if (locations[i][3] == 'zone01') {
+                //console.log('zone 1 เกกี');
+                console.log(locations[i][0])
+            }
+        }
     } else if (check_fbt == true) {
-        console.log('ตอนนี้คุณอยู่ในโซน fbt');
+        console.log('ตอนนี้คุณอยู่ในโซนเอฟบีที');
+        for (let i = 0; i < locations.length; i++) {
+            if (locations[i][3] == 'zone03') {
+                //console.log('zone 3 เอฟบีที');
+                console.log(locations[i][0])
+            }
+        }
     } else {
         console.log('คุณไม่อยู่ในพื้นที่ให้บริการ');
     }
